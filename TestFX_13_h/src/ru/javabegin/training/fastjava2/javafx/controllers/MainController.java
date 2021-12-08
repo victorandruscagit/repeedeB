56f175a667f733bc16ef3576187bca0b055b5f4c
victorandruscagit <victor.v.andrusca@gmail.com>
2021-12-08 17:16:22 +0200

    added @FXML componenents in edit mode modal window

--- TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
+++ TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
@@ -1,19 +1,55 @@
 package ru.javabegin.training.fastjava2.javafx.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.logging.Logger;
 
 public class MainController {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    private  Button btnDelete;
+
+    @FXML
+    private TableView tableAdressBook;
+
+    @FXML
+    private Label labelCount;
+
+    @FXML
+    private TextField txtSearch;
+
+
+
+
+
+
+
+
     public void showDialog(ActionEvent event) {
         try {
+            btnAdd.setText("apasat");
             Stage stage = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("../fxml/edit.fxml"));
             stage.setTitle("Redactarea inregistrarea");
