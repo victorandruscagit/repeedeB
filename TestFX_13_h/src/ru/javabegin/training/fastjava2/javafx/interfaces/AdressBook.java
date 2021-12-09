5125da6cfaa6e395fc2d77c7fa3d4cbccf5b1daa
victorandruscagit <victor.v.andrusca@gmail.com>
2021-12-09 18:03:31 +0200

    added add/delete/print/

---
+++ TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/interfaces/AdressBook.java
@@ -0,0 +1,10 @@
+package ru.javabegin.training.fastjava2.javafx.interfaces;
+
+import ru.javabegin.training.fastjava2.javafx.objects.Person;
+
+public interface AdressBook {
+    void add(Person person);
+    void update(Person person);
+    void delete(Person person);
+
+}
