8f26c59e7ac1176f81f0b26cb0f368d10b6f1bae
victorandruscagit <victor.v.andrusca@gmail.com>
2021-12-10 17:40:07 +0200

    ObservableList, TableView debuged

--- TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
+++ TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/controllers/MainController.java
@@ -1,23 +1,29 @@
 package ru.javabegin.training.fastjava2.javafx.controllers;
 
+import javafx.beans.property.Property;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import ru.javabegin.training.fastjava2.javafx.interfaces.impls.CollectionAddressBook;
+import ru.javabegin.training.fastjava2.javafx.objects.Person;
 
 import java.awt.*;
 import java.io.IOException;
 import java.util.logging.Logger;
 
 public class MainController {
+
+    private CollectionAddressBook addressBookImpl = new CollectionAddressBook();
 
     @FXML
     private Button btnAdd;
@@ -29,7 +35,7 @@ public class MainController {
     private Button btnEdit;
 
     @FXML
-    private  Button btnDelete;
+    private Button btnDelete;
 
     @FXML
     private TableView tableAdressBook;
@@ -39,17 +45,29 @@ public class MainController {
 
     @FXML
     private TextField txtSearch;
+    @FXML
+    private TableColumn<Person, String> columnFio;
+
+    @FXML
+    private TableColumn<Person, String> columnPhone;
+
+    @FXML
+    private void initialize() {
+        columnFio.setCellValueFactory(new PropertyValueFactory<Person, String>("fio"));
+        columnPhone.setCellValueFactory(new PropertyValueFactory<Person, String>("phone"));
+        addressBookImpl.feedTestData();
+        tableAdressBook.setItems(addressBookImpl.getPersonlist());
+        updateCountLabel();
 
 
+    }
 
-
-
-
-
+    private void updateCountLabel() {
+        labelCount.setText("Numar de inregistrari: " + addressBookImpl.getPersonlist().size());
+    }
 
     public void showDialog(ActionEvent event) {
         try {
-            btnAdd.setText("apasat");
             Stage stage = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("../fxml/edit.fxml"));
             stage.setTitle("Redactarea inregistrarea");
