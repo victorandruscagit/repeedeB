8cb3ecf44d1eff65af32b0d93f55a2c68cce5b98
victorandruscagit <victor.v.andrusca@gmail.com>
2021-12-15 09:32:45 +0200

    added locale resource bundle

--- TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/start/Main.java
+++ TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/start/Main.java
@@ -9,15 +9,22 @@ import ru.javabegin.training.fastjava2.javafx.controllers.MainController;
 import ru.javabegin.training.fastjava2.javafx.interfaces.impls.CollectionAddressBook;
 import ru.javabegin.training.fastjava2.javafx.objects.Person;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("../fxml/main.fxml"));
+        fxmlLoader.setResources(ResourceBundle.getBundle("ru.javabegin.training.fastjava2.javafx.bundles.Locale",new Locale("en")));
+
         Parent fxmlMain = fxmlLoader.load();
         MainController mainController = fxmlLoader.getController();
         mainController.setMainStage(primaryStage);
+
+        primaryStage.setTitle(fxmlLoader.getResources().getString("key.address_book"));
 
         primaryStage.setTitle("Address Book");
         primaryStage.setMinHeight(600);
