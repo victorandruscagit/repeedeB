fb6915c47f9d13b3a252a55cbe1252cdcba0d8c5
victorandruscagit <victor.v.andrusca@gmail.com>
2021-12-17 16:44:15 +0200

    disaster

---
+++ TestFX_13_h/src/ru/javabegin/training/fastjava2/javafx/objects/Lang.java
@@ -0,0 +1,54 @@
+package ru.javabegin.training.fastjava2.javafx.objects;
+
+import java.util.Locale;
+
+public class Lang {
+    private String code;
+    private String name;
+    private Locale locale;
+    private int index;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Lang(String code, String name, Locale locale, int index) {
+        this.code = code;
+        this.name = name;
+        this.locale = locale;
+        this.index = index;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
